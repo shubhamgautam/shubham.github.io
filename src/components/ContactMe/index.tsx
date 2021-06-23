d0b9501dea76eb683fa5c102273d40ad9e9e5767
shubhamgautam <shubhakmgautam@github,com>
2021-06-24 03:23:17 +0530

    updating social media link and nav

---
+++ src/components/ContactMe/index.tsx
@@ -0,0 +1,16 @@
+import "./index.css";
+
+import * as React from "react";
+
+const ContactMe  = () => {
+    return <div className="contact-me">
+            <ul className="contact-me-list">
+                <li></li>
+                <li></li>
+                <li className="mail-me font-mono"> <a href="mailto:shubhamgautam89@gmail.com" aria-label="mail to shubham" target="_black" rel="noreferrer">Shuhbamgautam89@gmail.com</a></li>
+                <li></li>
+            </ul>
+        </div>
+};
+
+export default ContactMe;
--- src/components/SocilaMediaLink/index.tsx
+++ src/components/SocilaMediaLink/index.tsx
@@ -17,33 +17,30 @@ const SocilaMediaBar = () => {
     return <div className="social-media-bar">
         <ul className="social-icon-list">
             <li></li>
-            <li></li>
-            <li></li>
             <li>
-                <GithubOutlined style={{ color: "white" }} />
+                <a href="https://github.com/shubhamgautam" aria-label="github" target="_blank" rel="noreferrer"><GithubOutlined className="social-media-icons"/></a>
             </li>
             <li>
-                <TwitterOutlined style={{ color: "white" }} />
+                <a href="https://twitter.com/notoriousShubh" aria-label="twitter" target="_blank" rel="noreferrer"><TwitterOutlined className="social-media-icons"/></a>
             </li>
             <li>
-                <InstagramOutlined style={{ color: "white" }} />
+                <a href="https://www.instagram.com/thenotoriousshubh/" aria-label="Instagram" target="_blank" rel="noreferrer"><InstagramOutlined className="social-media-icons"/></a>
             </li>
             <li>
-                <YoutubeOutlined style={{ color: "white" }} />
+                <a href="https://www.youtube.com/channel/UC9IFOtmDZ9bouTV4jd6YWNA" aria-label="Youtube" target="_blank" rel="noreferrer"><YoutubeOutlined className="social-media-icons"/></a>
             </li>
             <li>
-                <LinkedinOutlined style={{ color: "white" }} />
+                <a href="https://www.linkedin.com/in/shubham-gautam-aa370a2b/" aria-label="Linkedin" target="_blank" rel="noreferrer"><LinkedinOutlined className="social-media-icons"/></a>
             </li>
             <li>
-                <CodepenCircleOutlined style={{ color: "white" }} />
+                <a href="https://codepen.io/shubhamgautam" aria-label="Codepen" target="_blank" rel="noreferrer"><CodepenCircleOutlined className="social-media-icons"/></a>
             </li>
             <li>
-                <CodeSandboxOutlined style={{ color: "white" }} />
+                <a href="https://codesandbox.io/u/shubhamgautam" aria-label="Codesandbox" target="_blank" rel="noreferrer"><CodeSandboxOutlined className="social-media-icons"/></a>
             </li>
             <li>
-                <MediumOutlined style={{ color: "white" }} />
+                <a href="https://shubhamgautamlog.medium.com/" aria-label="Medium" target="_blank" rel="noreferrer"><MediumOutlined className="social-media-icons"/></a>
             </li>
-            <li></li>
             <li></li>
         </ul>
     </div>;
